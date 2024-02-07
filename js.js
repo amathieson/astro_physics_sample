1ec870121eb47454d9384c398228be13aa10fb61
Adam Mathieson <adam@mathieson.dev>
2024-02-07 16:17:47 +0000

    + a semi-functional demo

--- js.js
+++ js.js
@@ -9,6 +9,7 @@ let heading = document.getElementById("heading");
 
 let controller;
 let lastLoc = {latitude:0, longitude:0};
+let NorthOff = 0;
 
 start.addEventListener("click", ()=>{
     navigator.geolocation.watchPosition((pos)=>{
@@ -25,74 +26,53 @@ start.addEventListener("click", ()=>{
         });
 
     setInterval(()=>{
-        time.innerText = Date.now();
-        orientation.innerText = JSON.stringify(controller.getScreenAdjustedEuler());
-        sun.innerText = JSON.stringify(sun_pos(lastLoc, Date.now()));
+        time.innerText = new Date();
+        orientation.innerText = JSON.stringify(controller?.getScreenAdjustedEuler());
+        sun.innerText = JSON.stringify(sun_pos(lastLoc));
+        heading.innerText = (360-controller?.getScreenAdjustedEuler().alpha) + NorthOff;
     })
 })
 
-const JD2000 = 2451545.0;
-const RAD = Math.PI / 180.0;
-const DEG = 180.0 / Math.PI;
-function sun_pos(location, t) {
-    let time = new Date(t);
-    let longitude_deg = location.longitude;
-    let latitude_deg = location.latitude;
-    let year = time.getUTCFullYear();
-    let month = time.getUTCMonth();
-    let day = time.getDate();
-    let hour = time.getUTCHours();
-    let minute = time.getUTCMinutes();
-    let second = time.getUTCSeconds();
-    if (month <= 2) {
-        year -= 1
-        month += 12
-    }
-    let century = Math.floor(year / 100);
-    let Leap_Years = 2 - century + Math.floor(century/4)
-    let JD = Math.round(365.25 * (year + 4716)) + Math.round(30.6001 * (month + 1)) + day + Leap_Years - 1524.5 + (hour + minute / 60.0 + second / 3600.0) / 24.0
-    let Julian_DateStamp = JD - JD2000
-    let GMST = 18.697374558 + 24.06570982441908 * Julian_DateStamp
-    GMST %= 24
-    if (GMST < 0)
-        GMST += 24
+reset.addEventListener("click", ()=>{
+    NorthOff = -(360-controller?.getScreenAdjustedEuler().alpha) + sun_pos(lastLoc).azimuth;
+})
 
-    // Sun's mean longitude
-    let L = (280.46646 + 36000.76983 * Julian_DateStamp) % 360
-    if (L < 0)
-        L += 360
+const RAD = Math.PI / 180
+const DEG = 180 / Math.PI
+function sun_pos(location) {
+    const time = new Date();
+    const longitude_deg = location.longitude;
+    const latitude_deg = location.latitude;
+    const year = time.getFullYear();
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+    const sec = time.getSeconds();
+    const start = new Date(year, 0, 0);
+    const diff = time - start;
+    const oneDay = 1000 * 60 * 60 * 24;
+    const day_of_year = Math.floor(diff / oneDay);
+    const days_in_year = ((0 === year % 4) && (0 !== year % 100) || (0 === year % 400)) ? 366 : 365;
 
+    let altitude = Number.NaN;
+    let azimuth = Number.NaN;
 
-    // Sun's mean anomaly
-    let M = (357.52911 + 35999.05029 * Julian_DateStamp) % 360
-    if (M < 0)
-        M += 360
+    let frac_year = (2*Math.PI)/(days_in_year) *  (day_of_year - 1 + (hour-12)/24)
 
-    let lambda_sun = L + 1.914602 * Math.sin(M * RAD) + 0.019993 * Math.sin(2 * M * RAD)
-    lambda_sun %= 360
-    if (lambda_sun < 0)
-        lambda_sun += 360
+    let equi_time = 229.18 * (0.000075 + 0.001868*Math.cos(frac_year) - 0.032077*Math.sin(frac_year) - 0.014615 * Math.cos(2*frac_year) - 0.040849*Math.sin(2*frac_year));
 
+    let decl = 0.006918 - 0.399912*Math.cos(frac_year) + 0.070257*Math.sin(frac_year) - 0.00675*Math.cos(2*frac_year) + 0.000907 * Math.sin(2 * frac_year) - 0.002697*Math.cos(3 * frac_year) + 0.00148 * Math.sin(3 * frac_year);
 
-    // Sun's right ascension
-    let alpha_sun = Math.atan2(Math.sin(lambda_sun * RAD) * Math.cos(23.439292 * RAD), Math.cos(lambda_sun * RAD)) * DEG
-    if (alpha_sun < 0)
-        alpha_sun += 360
+    let time_offset = equi_time + 4*longitude_deg - 60*(time.getTimezoneOffset())
 
-    // Sun's declination
-    let delta_sun = Math.asin(Math.sin(23.439292 * RAD) * Math.sin(lambda_sun * RAD)) * DEG
+    let tst = hour * 60 + minute + sec/60 + time_offset;
 
-    // Hour angle
-    let hour_angle = GMST * 15 + longitude_deg - alpha_sun
-    if (hour_angle < -180)
-        hour_angle += 360
-    else if (hour_angle > 180)
-        hour_angle -= 360
+    let hour_angle = tst / 4 - 180;
 
-    // Altitude and azimuth
-    let altitude = Math.asin(Math.sin(latitude_deg * RAD) * Math.sin(delta_sun * RAD) + Math.cos(latitude_deg * RAD) * Math.cos(delta_sun * RAD) * Math.cos(hour_angle * RAD)) * DEG
-    let azimuth = Math.atan2(-Math.sin(hour_angle * RAD), Math.cos(hour_angle * RAD) * Math.sin(latitude_deg * RAD) - Math.tan(delta_sun * RAD) * Math.cos(latitude_deg * RAD)) * DEG
-    if (azimuth < 0)
-        azimuth += 360
+    let zenith = Math.acos(Math.sin(latitude_deg * RAD)*Math.sin(decl) + Math.cos(latitude_deg * RAD)*Math.cos(decl)*Math.cos(hour_angle * RAD));
+
+    azimuth = 180 + Math.acos((Math.sin(latitude_deg * RAD)*Math.cos(zenith) - Math.sin((decl))) / (Math.cos(latitude_deg * RAD) * Math.sin(zenith))) * DEG
+
+    altitude = 90 - zenith * DEG
+    // Return altitude and azimuth
     return {altitude, azimuth};
 }
